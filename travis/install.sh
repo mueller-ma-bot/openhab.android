afa0c4f7769f8bc09a7919cef0d6867c02c0d0c2
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-03 11:56:38 +0100

    Be less verbose

--- travis/install.sh
+++ travis/install.sh
@@ -1,4 +1,4 @@
-echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2"
-echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2"
-./gradlew --version
-sdkmanager --list || true
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout;1.0.2" > /dev/null
+echo yes | sdkmanager "extras;m2repository;com;android;support;constraint;constraint-layout-solver;1.0.2" > /dev/null
+#./gradlew --version
+#sdkmanager --list || true
