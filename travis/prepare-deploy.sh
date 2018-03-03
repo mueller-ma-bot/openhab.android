5189189e79bbdea023d72ca8e6f36396b753212c
mueller-ma <mueller-ma@users.noreply.github.com>
2018-03-03 12:09:00 +0100

    Correct gradlew commands

--- travis/prepare-deploy.sh
+++ travis/prepare-deploy.sh
@@ -3,6 +3,9 @@ git config --local user.email "support@openhab.org"
 git add fastlane/*
 git commit -m "Update fastlane metadata"
 #git push origin :refs/heads/master
+
+./gradlew assemble
+
 openssl aes-256-cbc -K $encrypted_903a93ed2309_key -iv $encrypted_903a93ed2309_iv -in keystore.enc -out keystore -d
 cp $TRAVIS_BUILD_DIR/keystore $HOME
 mkdir $HOME/apks_to_deploy
